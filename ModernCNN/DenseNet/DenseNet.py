9069c3de741189259eda8a96586b8b86c1231e74
cxz <1838038611@qq.com>
2022-12-26 11:30:43 +0800

    update

--- ModernCNN/DenseNet/DenseNet.py
+++ ModernCNN/DenseNet/DenseNet.py
@@ -28,3 +28,28 @@ def transition_block(input_channels, num_channels):
                          nn.Conv2d(input_channels, num_channels, kernel_size=1), nn.AvgPool2d(kernel_size=2, stride=2))
 
 
+class DenseNet(nn.Module):
+    def __init__(self, num_channels, growth_rate, num_convs_in_dense_blocks=[4, 4, 4, 4], **kwargs):
+        super(DenseNet, self).__init__(**kwargs)
+        b1 = nn.Sequential(
+            nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3),
+            nn.BatchNorm2d(64), nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1))
+        blks = []
+        for i, num_convs in enumerate(num_convs_in_dense_blocks):
+            blks.append(DenseBlock(num_convs, num_channels, growth_rate))
+            # 上⼀个稠密块的输出通道数
+            num_channels += num_convs * growth_rate
+            # 在稠密块之间添加⼀个转换层，使通道数量减半
+            if i != len(num_convs_in_dense_blocks) - 1:
+                blks.append(transition_block(num_channels, num_channels // 2))
+                num_channels = num_channels // 2
+        self.net = nn.Sequential(
+            b1, *blks,
+            nn.BatchNorm2d(num_channels), nn.ReLU(),
+            nn.AdaptiveMaxPool2d((1, 1)),
+            nn.Flatten(),
+            nn.Linear(num_channels, 10))
+
+    def forward(self, X):
+        return self.net(X)
