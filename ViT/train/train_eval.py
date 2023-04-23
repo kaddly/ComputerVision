bb8dfc5c386cf89de09bf73c1bd9f3e402569719
cxz <1838038611@qq.com>
2023-04-23 21:34:26 +0800

    update

--- ViT/train/train_eval.py
+++ ViT/train/train_eval.py
@@ -1,14 +1,20 @@
 import os
 import sys
-import torch
+import math
+import time
+import datetime
 from tqdm import tqdm
+import torch
+from torch import optim
+import torch.optim.lr_scheduler as lr_scheduler
+from torch.utils.tensorboard import SummaryWriter
 
 
 def train_one_epoch(model, optimizer, data_loader, device, epoch):
     model.train()
     loss_function = torch.nn.CrossEntropyLoss()
     accu_loss = torch.zeros(1).to(device)  # 累计损失
-    accu_num = torch.zeros(1).to(device)   # 累计预测正确的样本数
+    accu_num = torch.zeros(1).to(device)  # 累计预测正确的样本数
     optimizer.zero_grad()
 
     sample_num = 0
@@ -44,7 +50,7 @@ def evaluate(model, data_loader, device, epoch):
 
     model.eval()
 
-    accu_num = torch.zeros(1).to(device)   # 累计预测正确的样本数
+    accu_num = torch.zeros(1).to(device)  # 累计预测正确的样本数
     accu_loss = torch.zeros(1).to(device)  # 累计损失
     sample_num = 0
     data_loader = tqdm(data_loader, file=sys.stdout)
@@ -64,3 +70,66 @@ def evaluate(model, data_loader, device, epoch):
                                                                                accu_num.item() / sample_num)
 
     return accu_loss.item() / (step + 1), accu_num.item() / sample_num
+
+
+def train_eval(model, train_loader, val_loader, args):
+    results_file = os.path.join("results", args.dataset,
+                                "results{}.txt".format(datetime.datetime.now().strftime("%Y%m%d-%H%M%S")))
+    tb_writer = SummaryWriter()
+    device = torch.device(args.device if torch.cuda.is_available() else "cpu")
+    model = model.to(device)
+
+    if args.weights != "":
+        assert os.path.exists(args.weights), "weights file: '{}' not exist.".format(args.weights)
+        weights_dict = torch.load(args.weights, map_location=device)
+        # 删除不需要的权重
+        del_keys = ['head.weight', 'head.bias'] if model.has_logits else ['pre_logits.fc.weight', 'pre_logits.fc.bias',
+                                                                          'head.weight', 'head.bias']
+        for k in del_keys:
+            del weights_dict[k]
+        print(model.load_state_dict(weights_dict, strict=False))
+
+    if args.freeze_layers:
+        for name, para in model.named_parameters():
+            # 除head, pre_logits外，其他权重全部冻结
+            if "head" not in name and "pre_logits" not in name:
+                para.requires_grad_(False)
+            else:
+                print("training {}".format(name))
+
+    pg = [p for p in model.parameters() if p.requires_grad]
+    optimizer = optim.SGD(pg, lr=args.lr, momentum=0.9, weight_decay=5E-5)
+    # Scheduler https://arxiv.org/pdf/1812.01187.pdf
+    lf = lambda x: ((1 + math.cos(x * math.pi / args.epochs)) / 2) * (1 - args.lrf) + args.lrf  # cosine
+    scheduler = lr_scheduler.LambdaLR(optimizer, lr_lambda=lf)
+    for epoch in range(args.epochs):
+        # train
+        train_loss, train_acc = train_one_epoch(model=model,
+                                                optimizer=optimizer,
+                                                data_loader=train_loader,
+                                                device=device,
+                                                epoch=epoch)
+
+        scheduler.step()
+
+        # validate
+        val_loss, val_acc = evaluate(model=model,
+                                     data_loader=val_loader,
+                                     device=device,
+                                     epoch=epoch)
+        lr = optimizer.param_groups[0]["lr"]
+
+        with open(results_file, "a") as f:
+            train_info = f"[epoch: {epoch}]\n" \
+                         f"train_loss: {train_loss:>5.4f},  lr: {lr:>5.4f}\n"
+            val_info = f"val_loss: {val_loss:>5.4f}"
+            f.write(train_info + val_info + "\n\n")
+
+        tags = ["train_loss", "train_acc", "val_loss", "val_acc", "learning_rate"]
+        tb_writer.add_scalar(tags[0], train_loss, epoch)
+        tb_writer.add_scalar(tags[1], train_acc, epoch)
+        tb_writer.add_scalar(tags[2], val_loss, epoch)
+        tb_writer.add_scalar(tags[3], val_acc, epoch)
+        tb_writer.add_scalar(tags[4], lr, epoch)
+
+        torch.save(model.state_dict(), "./models/model-{}.pth".format(epoch))
